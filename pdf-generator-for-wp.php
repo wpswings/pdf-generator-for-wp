4cae897fa31c80bba4596000372903251d4ce8f5
puneet1450372 <yadavpuneet766@gmail.com>
2022-02-25 16:16:24 +0530

    changes

--- pdf-generator-for-wp.php
+++ pdf-generator-for-wp.php
@@ -269,6 +269,53 @@ function wps_pdf_gen_plugin_upgrade_notice() {
 		<?php
 	}
 }
+add_action( 'admin_init' , 'pgfw_pro_upgrade_wp_options' );
+	/**
+		 * Upgrade_wp_options. (use period)
+		 *
+		 * Upgrade_wp_options.
+		 *
+		 * @since    1.0.0
+		 */
+ function pgfw_pro_upgrade_wp_options() {
 
+	$condition = get_option('pgfw_wps_code_migrated');
+
+	if( $condition != 'yes') {
+		
+			$wp_options = array(
+				'mwb_pgfw_onboarding_data_skipped' => '',
+				'mwb_all_plugins_active'		   => '',
+				'mwb_pgfw_onboarding_data_sent'	   => '',
+				'pgfw_wps_code_migrated'		=> 'yes',
+
+			);
+
+			foreach ( $wp_options as $key => $value ) {
+
+				$new_key = str_replace( 'mwb_', 'wps_', $key );	
+				if ( ! empty( get_option( $new_key ) ) ) {
+					continue;
+				}
+				$new_value = get_option( $key, $value );
+
+				$arr_val = array();
+				if ( is_array( $new_value )) {
+					foreach ( $new_value as $key => $value) {
+						$new_key1 = str_replace( 'mwb_', 'wps_', $key );
+						$new_key2 = str_replace( 'mwb-', 'wps-', $new_key1 );
+
+						$value_1 = str_replace( 'mwb-', 'wps-', $value);
+						$value_2 = str_replace( 'mwb_', 'wps_', $value_1);
+						$arr_val[ $new_key2 ] = $value_2;
+					}
+					update_option( $new_key, $arr_val );
+				}
+				else {
+					update_option( $new_key, $new_value );
+				}
+			}
+		}
+		}
 ///////////////////Ending noticed code/////////////////////////////////////
 
