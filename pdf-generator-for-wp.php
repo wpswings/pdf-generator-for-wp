8c4fc4efc621e4fb9aa60ac18f3db2d77ec8422d
Puneet-cedcoss <79438024+Puneet-cedcoss@users.noreply.github.com>
2022-05-19 17:36:03 +0530

    Update pdf-generator-for-wp.php

--- pdf-generator-for-wp.php
+++ pdf-generator-for-wp.php
@@ -22,9 +22,9 @@
  * Domain Path:       /languages
  *
  * Requires at least:    4.6
- * Tested up to:         5.9.3
+ * Tested up to:         6.0.0
  * WC requires at least: 4.0.0
- * WC tested up to:      6.4.1
+ * WC tested up to:      6.5.1
  * Stable tag:           1.0.7
  * Requires PHP:         7.2
  *
