d59947bb0c157dcd74147a67dbea2944a2cb953e
Vipul <vipul@cedcoss.com>
2023-05-17 12:17:04 +0530

    updated

---
+++ admin/partials/pdf_templates/pdf-generator-for-wp-invoice-pdflayout2.php
@@ -0,0 +1,292 @@
+<?php
+/**
+ * This is template one for the pdf generation.
+ *
+ * @package pdf_generator_for_wp.
+ *
+ * @return void
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+/**
+ * Return the html data for the second template.
+ *
+ * @param int    $order_id order id.
+ * @param string $type packing slip or the invoice.
+ * @param string $invoice_id current invoice ID.
+ * @return string
+ */
+function return_ob_value( $order_id, $type, $invoice_id ) {
+	
+	
+	$order_details         = do_shortcode( '[WPG_FETCH_ORDER order_id ="' . $order_id . '"]' );
+
+	$order_details         = json_decode( $order_details, true );
+	
+	$shipping_details      = $order_details['shipping_details'];
+	$billing_details       = $order_details['billing_details'];
+	$order_product_details = $order_details['product_details'];
+	$company_name          = get_option( 'wpg_company_name' );
+	$company_address       = get_option( 'wpg_company_address' );
+	$company_city          = get_option( 'wpg_company_city' );
+	$company_state         = get_option( 'wpg_company_state' );
+	$company_pin           = get_option( 'wpg_company_pin' );
+	$company_phone         = get_option( 'wpg_company_phone' );
+	$company_email         = get_option( 'wpg_company_email' );
+	$digit                 = get_option( 'wpg_invoice_number_digit' );
+	$prefix                = get_option( 'wpg_invoice_number_prefix' );
+	$suffix                = get_option( 'wpg_invoice_number_suffix' );
+	$date                  = get_option( 'wpg_invoice_renew_date' );
+	$disclaimer            = get_option( 'wpg_invoice_disclaimer' );
+	$color                 = get_option( 'wpg_invoice_color' );
+	$is_add_logo           = get_option( 'wpg_is_add_logo_invoice' );
+	$logo                  = get_option( 'sub_wpg_upload_invoice_company_logo' );
+	$digit                 = ( $digit ) ? $digit : 3;
+	$color                 = ( $color ) ? $color : '#000000';
+	if ( $order_details ) {
+		$html = '<!DOCTYPE html>
+					<html>
+					<head>
+						<title> INVOICE SYSTEM FOR WOOCOMMERCE </title>
+						<style>
+							#wpg-pdf-header {
+								margin-bottom: -80px;
+							}
+							#wpg-invoice-title-right {
+								margin-top: -20px;
+							}
+					
+							.wpg-invoice-inline {
+								display: inline-block;
+								width: 50%;
+							}
+							table#wpg-prod-listing-table{
+								margin-top: 40px;
+								width: 100%;
+							}
+							table#wpg-prod-listing-table tr th {
+								text-align: center;
+								padding: 5px;
+							}
+							table#wpg-prod-listing-table tr #wpg-table-items {
+								text-align: left;
+							}
+					
+							#wpg-pdf-prod-body tr td {
+								text-align: center;
+								padding: 15px 0;
+							}
+							#wpg-pdf-prod-body tr .wpg-product-name {
+								text-align: left;
+							}
+							#wpg-prod-listing-table-title {
+								background-color: ' . $color . ';
+								color: white;
+							}
+							#wpg-prod-listing-table-bottom {
+								border-bottom: 2px solid black;
+								margin-top: 20px;
+								position: relative;
+							}
+							
+							#wpg-invoice-text{
+								color: ' . $color . ';
+								margin-bottom: 30px;
+							}
+					
+							#wpg-prod-total-calc table {
+								text-align: right;
+								table-layout: fixed;
+								width: 96%;
+								margin-top: 30px;
+							}
+							.wpg-invoice-greetings {
+								margin-top: 40px;
+							}
+							#wpg-invoice-title-right {
+								margin-top: -20px;
+								text-align: right;
+							}
+							#wpg-pdf{
+								font-family: DejaVu Sans !important;
+							}
+						</style>
+					</head>
+					<body>
+						<div id="wpg-pdf">
+							<h2 id="wpg-invoice-text">
+							' . __( 'INVOICE', 'invoice-system-for-woocommerce' ) . '
+							</h2>
+							<div id="wpg-pdf-header">
+								<div id="wpg-invoice-title-left" class="wpg-invoice-inline">
+									<div>
+										<b>' . __( 'Invoice Number', 'invoice-system-for-woocommerce' ) . '</b><br/>
+										' . $invoice_id . '
+									</div>
+									<div>
+										<b>' . __( 'Date', 'invoice-system-for-woocommerce' ) . '</b><br/>
+										' . $billing_details['order_created_date'] . '
+									</div>
+								</div>
+								<div id="wpg-invoice-title-right" class="wpg-invoice-inline">
+									<div>';
+		if ( 'yes' === $is_add_logo && '' !== $logo ) {
+			$html .= '<img src="' . $logo . '" height="120" width="120"><br/>';
+		}
+		if ( $company_name ) {
+			$html .= '<b>' . ucfirst( $company_name ) . '</b><br/>';
+		}
+		if ( $company_address ) {
+			$html .= ucfirst( $company_address ) . ' ,';
+		}
+		if ( $company_city ) {
+			$html .= ucfirst( $company_city ) . '<br/>';
+		}
+		if ( $company_state ) {
+			$html .= ucfirst( $company_state );
+		}
+		if ( $company_pin ) {
+			$html .= '<br/> ' . $company_pin . '<br/>';
+		}
+		if ( $company_phone ) {
+			$html .= $company_phone . '<br/>';
+		}
+		if ( $company_email ) {
+			$html .= $company_email;
+		}
+		$html .= '</div>
+			</div>
+		</div>';
+		if ( 'invoice' === $type ) {
+			$html .= '<div id="wpg-invoice-title-to" >
+						<b>' . __( 'Invoice to', 'invoice-system-for-woocommerce' ) . '</b><br/>
+						<div>
+							' . ucfirst( $billing_details['billing_first_name'] ) . ' ' . ucfirst( $billing_details['billing_last_name'] ) . '<br/>';
+			if ( $billing_details['billing_company'] ) {
+				$html .= $billing_details['billing_company'] . '<br/>';
+			}
+			if ( $billing_details['billing_address_1'] ) {
+				$html .= ucfirst( $billing_details['billing_address_1'] ) . ' ' . ucfirst( $billing_details['billing_address_2'] ) . '<br/>';
+			}
+			if ( $billing_details['billing_city'] ) {
+				$html .= ucfirst( $billing_details['billing_city'] ) . '<br/>';
+			}
+			if ( $billing_details['billing_state'] ) {
+				$html .= ucfirst( $billing_details['billing_state'] ) . '<br/>';
+			}
+			if ( $billing_details['billing_postcode'] ) {
+				$html .= $billing_details['billing_postcode'] . '<br/>';
+			}
+			if ( $billing_details['billing_phone'] ) {
+				$html .= $billing_details['billing_phone'] . '<br/>';
+			}
+			if ( $billing_details['billing_email'] ) {
+				$html .= $billing_details['billing_email'] . '<br/>';
+			}
+			$html .= '</div>
+			</div>';
+		} else {
+			$html .= '<div id="wpg-invoice-title-to" >
+						<b>' . __( 'SHIP TO', 'invoice-system-for-woocommerce' ) . '</b><br/>
+						<div>
+							' . ucfirst( $shipping_details['shipping_first_name'] ) . ' ' . ucfirst( $shipping_details['shipping_last_name'] ) . '<br/>';
+			if ( $shipping_details['shipping_company'] ) {
+				$html .= $shipping_details['shipping_company'] . '<br/>';
+			}
+			if ( $shipping_details['shipping_address_1'] ) {
+				$html .= ucfirst( $shipping_details['shipping_address_1'] ) . ' ' . ucfirst( $shipping_details['shipping_address_2'] ) . '<br/>';
+			}
+			if ( $shipping_details['shipping_city'] ) {
+				$html .= ucfirst( $shipping_details['shipping_city'] ) . '<br/>';
+			}
+			if ( $shipping_details['shipping_state'] ) {
+				$html .= ucfirst( $shipping_details['shipping_state'] ) . '<br/>';
+			}
+			if ( $shipping_details['shipping_postcode'] ) {
+				$html .= $shipping_details['shipping_postcode'] . '<br/>';
+			}
+			if ( $billing_details['billing_phone'] ) {
+				$html .= $billing_details['billing_phone'] . '<br/>';
+			}
+			if ( $billing_details['billing_email'] ) {
+				$html .= $billing_details['billing_email'] . '<br/>';
+			}
+			$html .= '</div>
+					</div>';
+		}
+		if ( 'invoice' === $type ) {
+			$html .= '<div>
+						<table border = "0" cellpadding = "0" cellspacing = "0" id="wpg-prod-listing-table">
+							<thead>
+								<tr id="wpg-prod-listing-table-title">
+									<th id="wpg-table-items">' . __( 'Items', 'invoice-system-for-woocommerce' ) . '</th>
+									<th>' . __( 'Quantity', 'invoice-system-for-woocommerce' ) . '</th>
+									<th>' . __( 'Price', 'invoice-system-for-woocommerce' ) . '(' . $billing_details['order_currency'] . ')</th>
+									<th>' . __( 'Tax', 'invoice-system-for-woocommerce' ) . ' (%)</th>
+									<th>' . __( 'Amount', 'invoice-system-for-woocommerce' ) . '(' . $billing_details['order_currency'] . ')</th>
+								</tr>
+							</thead>
+							<tbody id="wpg-pdf-prod-body">';
+			$meta_data = '';
+			foreach ( $order_product_details as $product ) {
+				$item_data = ! empty( $product['item_meta'] ) ? $product['item_meta'] : array();
+				if ( ! empty( $item_data ) && is_array( $item_data ) ) {
+					foreach ( $item_data as $key => $item ) {
+						
+						if ( 'is_upsell_purchase' === $item['display_key'] ) {
+							continue;
+						}
+
+						$meta_data .= '<br/>' . $item['display_key'] . ':' . $item['display_value'];
+					}
+				}
+				$html .= '<tr>
+						<td class="wpg-product-name">' . $product['product_name'] . $meta_data . '</td>
+						<td>' . $product['product_quantity'] . '</td>
+						<td>' . $product['product_price'] . '</td>
+						<td>' . $product['tax_percent'] . '</td>
+						<td>' . $product['product_total'] . '</td>
+					</tr>';
+			}
+			$html               .= '</tbody>
+										</table>
+										<div id="wpg-prod-listing-table-bottom"></div>
+										<div id="wpg-prod-total-calc">
+											<table border = "0" cellpadding = "0" cellspacing = "0">
+												<tr>
+													<td>' . __( 'Payment via', 'invoice-system-for-woocommerce' ) . ' : ' . $billing_details['payment_method'] . '</td>
+												</tr>
+												<tr>
+													<td>' . __( 'Subtotal', 'invoice-system-for-woocommerce' ) . '(' . $billing_details['order_currency'] . '): ' . $billing_details['order_subtotal'] . '</td>
+												</tr>
+												<tr>
+													<td>' . __( 'Shipping', 'invoice-system-for-woocommerce' ) . '(' . $billing_details['order_currency'] . '): ' . $shipping_details['shipping_total'] . '</td>
+												</tr>
+												<tr>
+													<td>' . __( 'Total tax', 'invoice-system-for-woocommerce' ) . '(' . $billing_details['order_currency'] . '): ' . $billing_details['tax_totals'] . '</td>
+												</tr>';
+			$wpg_coupon_details = $billing_details['coupon_details'];
+			foreach ( $wpg_coupon_details as $key => $price ) {
+				$html .= '<tr>
+							<td>' . $key . '(' . $billing_details['order_currency'] . '): ' . $price . '</td>
+						</tr>';
+			}
+			$html .= '<tr>
+								<td>' . __( 'Total', 'invoice-system-for-woocommerce' ) . '(' . $billing_details['order_currency'] . '): ' . $billing_details['cart_total'] . '</td>
+							</tr>
+						</table>
+					</div>
+					<div class="wpg-invoice-greetings">
+						<b>' . $disclaimer . '</b>
+					</div>
+					</div>';
+		}
+		$html .= '</div>
+				</body>
+			</html>';
+		return $html;
+	}
+	return '<div>' . esc_html__( 'Looks like order is not found', 'invoice-system-for-woocommerce' ) . '</div>';
+}
--- common/class-pdf-generator-for-wp-common.php
+++ common/class-pdf-generator-for-wp-common.php
@@ -580,6 +580,296 @@ class Pdf_Generator_For_Wp_Common {
 		return bulk_pdf_exporter_html( $ids );
 
 	}
+//////////////////////////////////////////////////invoice
 
+	/**
+	 * Schedular for resetting invoice number.
+	 *
+	 * @since 1.0.1
+	 * @return void
+	 */
+	public function wpg_reset_invoice_number_schedular() {
+		if ( ! as_next_scheduled_action( 'wpg_reset_invoice_number_hook' ) ) {
+			as_schedule_recurring_action( strtotime( 'tomorrow' ), DAY_IN_SECONDS, 'wpg_reset_invoice_number_hook' );
+		}
+	}
+	/**
+	 * Reset invoice number.
+	 *
+	 * @since 1.0.1
+	 * @return void
+	 */
+	public function wpg_reset_invoice_number() {
+		$month = get_option( 'wpg_invoice_number_renew_month' );
+		$date  = get_option( 'wpg_invoice_number_renew_date' );
+		if ( '' !== $month && 'never' !== $month ) {
+			if ( ( (int) current_time( 'm' ) === (int) $month ) && ( (int) current_time( 'd' ) === (int) $date ) ) {
+				update_option( 'wpg_current_invoice_id', 0 );
+			}
+		}
+	}
+	/**
+	 * Generate Invoice Number.
+	 *
+	 * @param int $order_id order ID to generate invoice number for.
+	 * @since 1.0.0
+	 * @return string
+	 */
+	public function wpg_invoice_number( $order_id ) {
+		$digit  = get_option( 'wpg_invoice_number_digit' );
+		$prefix = get_option( 'wpg_invoice_number_prefix' );
+		$suffix = get_option( 'wpg_invoice_number_suffix' );
+		$digit  = ( $digit ) ? $digit : 4;
+		$in_id  = get_post_meta( $order_id, 'wpg_order_invoice_id', true );
+		if ( $in_id ) {
+			$invoice_id = $in_id;
+		} else {
+			$prev_invoice_id = get_option( 'wpg_current_invoice_id' );
+			if ( $prev_invoice_id ) {
+				$curr_invoice_id = $prev_invoice_id + 1;
+			} else {
+				$curr_invoice_id = 1;
+			}
+			update_option( 'wpg_current_invoice_id', $curr_invoice_id );
+			$invoice_number = str_pad( $curr_invoice_id, $digit, '0', STR_PAD_LEFT );
+			$invoice_id     = $prefix . $invoice_number . $suffix;
+			update_post_meta( $order_id, 'wpg_order_invoice_id', $invoice_id );
+		}
+		return $invoice_id;
+	}
+	/**
+	 * Invoice name for the file to be stored or downloaded.
+	 *
+	 * @param string $type invoice or packing slip.
+	 * @param int    $order_id order id to generate invoice for.
+	 * @return string
+	 */
+	public function wpg_invoice_name_for_file( $type, $order_id ) {
+		$invoice_name_option = get_option( 'mwb_wpiwps_invoice_name' );
+		$invoice_id          = $this->wpg_invoice_number( $order_id );
+		if ( 'invoice' === $type ) {
+			if ( 'custom' === $invoice_name_option ) {
+				$custom_invoice_name = get_option( 'mwb_wpiwps_custom_invoice_name' );
+				$invoice_name        = $custom_invoice_name . '_' . $order_id;
+			} elseif ( 'invoice_orderid' === $invoice_name_option ) {
+				$invoice_name = 'invoice_' . $order_id;
+			} elseif ( 'invoice_id' === $invoice_name_option ) {
+				$invoice_name = $invoice_id;
+			} else {
+				$invoice_name = $type . '_' . $order_id;
+			}
+		} else {
+			$invoice_name = $type . '_' . $order_id;
+		}
+		return $invoice_name;
+	}
+
+	/**
+	 * Download file by passing file url.
+	 *
+	 * @param string $file_url url of the file to download.
+	 * @return void
+	 */
+	public function wpg_download_already_existing_invoice_file( $file_url ) {
+		@ob_end_clean(); // phpcs:ignore
+		header( 'Content-Type: application/octet-stream' );
+		header( 'Content-Transfer-Encoding: Binary' );
+		header( 'Content-disposition: attachment; filename="' . basename( $file_url ) . '"' );
+		readfile( $file_url ); // phpcs:ignore WordPress
+		exit;
+	}
+	/**
+	 * Common method for generating pdf.
+	 *
+	 * @param int    $order_id order id to print invoice for.
+	 * @param string $type type invoice or packing slip.
+	 * @param string $action either download locally or on server.
+	 * @return string
+	 */
+	public function wpg_common_generate_pdf( $order_id, $type, $action ) {
+		require_once PDF_GENERATOR_FOR_WP_DIR_PATH . 'package/lib/dompdf/vendor/autoload.php';
+		$wpg_invoice_template            = get_option( 'wpg_invoice_template' );
+		$wpg_generate_invoice_from_cache = get_option( 'wpg_generate_invoice_from_cache' );
+		$invoice_id                       = $this->wpg_invoice_number( $order_id );
+		$invoice_name                     = $this->wpg_invoice_name_for_file( $type, $order_id );
+		$upload_dir                       = wp_upload_dir();
+		$upload_basedir                   = $upload_dir['basedir'] . '/invoices/';
+		$path                             = $upload_basedir . $invoice_name . '.pdf';
+		$file_url                         = $upload_dir['baseurl'] . '/invoices/' . $invoice_name . '.pdf';
+		if ( ( 'yes' === $wpg_generate_invoice_from_cache ) && file_exists( $path ) ) {
+			if ( 'download_locally' === $action ) {
+				$this->wpg_download_already_existing_invoice_file( $file_url );
+			} elseif ( 'download_on_server' === $action ) {
+				return $path;
+			}
+		} else {
+			if ( $wpg_invoice_template ) {
+				$template = $wpg_invoice_template;
+			} else {
+				$template = 'one';
+			}
+			if ( 'one' === $template ) {
+				$template_path = PDF_GENERATOR_FOR_WP_DIR_PATH . 'admin/partials/pdf_templates/pdf-generator-for-wp-invoice-pdflayout1.php';
+			} else {
+				$template_path = PDF_GENERATOR_FOR_WP_DIR_PATH . 'admin/partials/pdf_templates/pdf-generator-for-wp-invoice-pdflayout2.php';
+			}
+			$template_path = apply_filters( 'wpg_load_template_for_invoice_generation', $template_path );
+			require_once $template_path;
+			$html   =  return_ob_value( $order_id, $type, $invoice_id );
+			
+			$dompdf = new Dompdf( array( 'enable_remote' => true ) );
+			$dompdf->loadHtml( $html );
+			$dompdf->setPaper( 'A4' );
+			@ob_end_clean(); // phpcs:ignore
+			$dompdf->render();
+			if ( ! file_exists( $upload_basedir ) ) {
+				wp_mkdir_p( $upload_basedir );
+			}
+			
+			if ( 'download_locally' === $action ) {
+				$output = $dompdf->output();
+				if ( file_exists( $path ) ) {
+					@unlink( $path ); // phpcs:ignore
+				}
+				if ( ! file_exists( $path ) ) {
+					@file_put_contents( $path, $output ); // phpcs:ignore
+				}
+				if ( 'invoice' === $type ) {
+					do_action( 'mwb_wpg_upload_invoice_in_storage', $path, $file_url, $order_id, $invoice_name );
+				}
+				$dompdf->stream( $invoice_name . '.pdf', array( 'Attachment' => 1 ) );
+			}
+			if ( 'open_window' === $action ) {
+				$output = $dompdf->output();
+				$dompdf->stream( $invoice_name . '.pdf', array( 'Attachment' => 0 ) );
+			}
+			if ( 'download_on_server' === $action ) {
+				$output = $dompdf->output();
+				if ( file_exists( $path ) ) {
+					@unlink( $path ); // phpcs:ignore
+				}
+				if ( ! file_exists( $path ) ) {
+					@file_put_contents( $path, $output ); // phpcs:ignore
+				}
+				if ( 'invoice' === $type ) {
+					do_action( 'mwb_wpg_upload_invoice_in_storage', $path, $file_url, $order_id, $invoice_name );
+				}
+				return $path;
+			}
+		}
+	}
+	/**
+	 * Adding shortcodes for fetching order details.
+	 *
+	 * @return void
+	 */
+	public function wpg_fetch_order_details_shortcode() {
+		add_shortcode( 'WPG_FETCH_ORDER', array( $this, 'wpg_fetch_order_details' ) );
+	}
+	/**
+	 * Fetching all order details and storing in array.
+	 *
+	 * @param array $atts attributes which are passed while using shortcode.
+	 * @return array
+	 */
+	public function wpg_fetch_order_details( $atts = array() ) {
+		$atts  = shortcode_atts(
+			array(
+				'order_id' => '',
+			),
+			$atts
+		);
+		$order = wc_get_order( $atts['order_id'] );
+		if ( is_a( $order, 'WC_Order_Refund' ) ) {
+			$order = wc_get_order( $order->get_parent_id() );
+		}
+		if ( $order ) {
+			$order_subtotal     = preg_replace( '/[^0-9,.]/', '', $order->get_subtotal() );
+			$decimal_separator  = wc_get_price_decimal_separator();
+			$thousand_separator = wc_get_price_thousand_separator();
+			$decimals           = wc_get_price_decimals();
+			$wpg_coupon_fee    = array();
+			$coupon_fees        = $order->get_fees();
+			if ( is_array( $coupon_fees ) ) {
+				foreach ( $coupon_fees as $item_fee ) {
+					$fee_name                     = $item_fee->get_name();
+					$fee_total                    = $item_fee->get_total();
+					$wpg_coupon_fee[ $fee_name ] = $fee_total;
+				}
+			}
+			$order_product_details = array();
+			foreach ( $order->get_items() as  $item_key => $item_values ) {
+				$_tax                    = new WC_Tax();
+				$item_data               = $item_values->get_data();
+				$product_tax             = $_tax->get_rates( $item_data['tax_class'] );
+				$product_tax             = is_array( $product_tax ) ? array_shift( $product_tax ) : $product_tax;
+				$product_tax             = is_array( $product_tax ) ? array_shift( $product_tax ) : $product_tax;
+				$product_tax             = ( $product_tax ) ? $product_tax : 0;
+				$order_product_details[] = array(
+					'product_id'       => get_post_meta( $item_data['product_id'], '_sku', true ),
+					'id'               => $item_data['product_id'],
+					'item_meta'        => $item_values->get_formatted_meta_data(),
+					'product_name'     => $item_data['name'],
+					'product_quantity' => $item_data['quantity'],
+					'product_price'    => ( 0 !== (int) $item_data['quantity'] ) ? number_format( ( preg_replace( '/,/', '.', $item_data['total'] ) / $item_data['quantity'] ), $decimals, $decimal_separator, $thousand_separator ) : 0,
+					'product_tax'      => number_format( preg_replace( '/,/', '.', $item_data['total_tax'] ), $decimals, $decimal_separator, $thousand_separator ),
+					'product_total'    => number_format( ( preg_replace( '/,/', '.', $item_data['total'] ) + preg_replace( '/,/', '.', $item_data['total_tax'] ) ), $decimals, $decimal_separator, $thousand_separator ),
+					'tax_percent'      => number_format( $product_tax, $decimals, $decimal_separator, $thousand_separator ),
+				);
+			}
+			$shipping_total          = preg_replace( '/[^0-9,.]/', '', $order->get_shipping_total() );
+			$shipping_total_format   = ( $shipping_total ) ? number_format( $shipping_total, $decimals, $decimal_separator, $thousand_separator ) : 0;
+			$shipping_tax            = preg_replace( '/[^0-9,.]/', '', $order->get_shipping_tax() );
+			$shipping_total_with_tax = ( $shipping_total ) ? number_format( ( $shipping_total + $shipping_tax ), $decimals, $decimal_separator, $thousand_separator ) : number_format( $shipping_tax, $decimals, $decimal_separator, $thousand_separator );
+			$shipping_details        = array(
+				'shipping_first_name'     => $order->get_shipping_first_name(),
+				'shipping_last_name'      => $order->get_shipping_last_name(),
+				'shipping_address_1'      => $order->get_shipping_address_1(),
+				'shipping_address_2'      => $order->get_shipping_address_2(),
+				'shipping_city'           => $order->get_shipping_city(),
+				'shipping_company'        => $order->get_shipping_company(),
+				'shipping_state'          => $order->get_shipping_state(),
+				'shipping_postcode'       => $order->get_shipping_postcode(),
+				'shipping_country'        => $order->get_shipping_country(),
+				'shipping_method'         => $order->get_shipping_method(),
+				'shipping_total'          => $shipping_total_format,
+				'shipping_tax'            => $shipping_tax,
+				'shipping_total_with_tax' => $shipping_total_with_tax,
+				'order_status'            => $order->get_status(),
+			);
+			$cart_total              = preg_replace( '/[^0-9,.]/', '', $order->get_total() );
+			$tax_total               = preg_replace( '/[^0-9,.]/', '', $order->get_total_tax() );
+			$billing_details         = array(
+				'coupon_details'     => $wpg_coupon_fee,
+				'customer_id'        => $order->get_customer_id(),
+				'billing_email'      => $order->get_billing_email(),
+				'billing_phone'      => $order->get_billing_phone(),
+				'billing_first_name' => $order->get_billing_first_name(),
+				'billing_last_name'  => $order->get_billing_last_name(),
+				'billing_company'    => $order->get_billing_company(),
+				'billing_address_1'  => $order->get_billing_address_1(),
+				'billing_address_2'  => $order->get_billing_address_2(),
+				'billing_city'       => $order->get_billing_city(),
+				'billing_state'      => $order->get_billing_state(),
+				'billing_postcode'   => $order->get_billing_postcode(),
+				'billing_country'    => $order->get_billing_country(),
+				'payment_method'     => $order->get_payment_method_title(),
+				'order_subtotal'     => number_format( $order_subtotal, $decimals, $decimal_separator, $thousand_separator ),
+				'order_currency'     => get_woocommerce_currency_symbol( $order->get_currency() ),
+				'cart_total'         => number_format( $cart_total, $decimals, $decimal_separator, $thousand_separator ),
+				'tax_totals'         => ( $tax_total ) ? number_format( $tax_total, $decimals, $decimal_separator, $thousand_separator ) : 0,
+				'order_created_date' => $order->get_date_created()->format( get_option( 'date_format', 'd-m-Y' ) ),
+			);
+			$payment = $order->get_checkout_payment_url();
+			$order_details_arr       = array(
+				'shipping_details' => $shipping_details,
+				'billing_details'  => $billing_details,
+				'product_details'  => $order_product_details,
+				'payment_url' => $payment,
+			);
+			return wp_json_encode( $order_details_arr );
+		}
+		return false;
+	}
 	
 }
