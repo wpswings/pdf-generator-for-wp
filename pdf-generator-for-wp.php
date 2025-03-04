15419a1f2803163c7a0d91a1bf1dc5a6d1475cf6
ananya-cedcoss <ananyashukla@makewebbetter.com>
2025-03-04 12:45:32 +0530

    issues fixed

--- pdf-generator-for-wp.php
+++ pdf-generator-for-wp.php
@@ -22,7 +22,7 @@
  * Domain Path:       /languages
  *
  * Requires at least:    5.5.0
- * Tested up to:         6.7.1
+ * Tested up to:         6.7.2
  * WC requires at least: 5.2.0
  * WC tested up to:      9.7.0
  * Stable tag:           1.4.1
