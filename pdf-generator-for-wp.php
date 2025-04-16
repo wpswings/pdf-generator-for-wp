611eff0d41d9cb8071facbb195a9a2c13c38fba7
prince-cedcoss <princekumaryadav@makewebbetter.com>
2025-04-16 13:40:40 +0530

    wp version update

--- pdf-generator-for-wp.php
+++ pdf-generator-for-wp.php
@@ -22,7 +22,7 @@
  * Domain Path:       /languages
  *
  * Requires at least:    5.5.0
- * Tested up to:         6.7.2
+ * Tested up to:         6.8.0
  * WC requires at least: 5.2.0
  * WC tested up to:      9.8.1
  * Stable tag:           1.5.0
